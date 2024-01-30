5e836f158d7b06fee225eca29dc36379b5fd9fdd
KarlitosTaquitos <chern4261@gmail.com>
2024-01-29 21:11:49 -0600

    field oriented drive base

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,71 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.SPI;
+
+public class DriveTrain extends SubsystemBase {
+  MecanumDrive mecanumDrive;
+
+  CANSparkMax frontLeftMotor, frontRightMotor, rearLeftMotor, rearRightMotor;
+
+  AHRS navx;
+
+  boolean fieldOriented = true;
+
+  /** Creates a new DriveTrain. */
+  public DriveTrain() {
+
+    // Drive motors
+    frontLeftMotor = new CANSparkMax(Constants.MotorControllerConstants.frontLeft, MotorType.kBrushless);
+    frontRightMotor = new CANSparkMax(Constants.MotorControllerConstants.frontRight, MotorType.kBrushless);
+    rearLeftMotor = new CANSparkMax(Constants.MotorControllerConstants.backLeft, MotorType.kBrushless);
+    rearRightMotor = new CANSparkMax(Constants.MotorControllerConstants.backRight, MotorType.kBrushless);
+
+    frontRightMotor.setInverted(true);
+    rearRightMotor.setInverted(true);
+
+    // Creating drive system
+    mecanumDrive = new MecanumDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+    mecanumDrive.setSafetyEnabled(false);
+
+    // Gyroscope
+    navx = new AHRS(SPI.Port.kMXP);
+    navx.reset();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void drive(double forward, double strafe, double turn) {
+    if (fieldOriented) {
+      System.out.println(navx.getRotation2d());
+      driveFieldOriented(forward, strafe, turn);
+    } else
+      driveRobotOriented(forward, strafe, turn);
+  }
+
+  public void driveFieldOriented(double forward, double strafe, double turn) {
+    mecanumDrive.driveCartesian(-forward, strafe, turn, navx.getRotation2d().unaryMinus());
+  }
+
+  public void driveRobotOriented(double forward, double strafe, double turn) {
+    mecanumDrive.driveCartesian(-forward, strafe, turn);
+  }
+
+  public void toggleFieldOrientedDrive() {
+    fieldOriented = !fieldOriented;
+  }
+}
