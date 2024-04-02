c994e6e2e3cdbbf799ee3e656c8f185fd8262e96
danielra12393 <danram1001@gmail.com>
2024-04-01 22:30:30 -0500

    Made methods to generate a bunch of paths for autos

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -107,56 +107,9 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // Create config for trajectory
-    TrajectoryConfig config =
-        new TrajectoryConfig(
-                AutoConstants.kMaxSpeedMetersPerSecond,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-            // Add kinematics to ensure max speed is actually obeyed
-            .setKinematics(OdometryConstants.mecanumDriveKinematics);
-            
-    // An example trajectory to follow. All units in meters.
-    Trajectory exampleTrajectory =
-        TrajectoryGenerator.generateTrajectory(
-            // Start at the origin facing the +X direction
-            new Pose2d(0, 0, new Rotation2d(0)),
-            // Pass through these two interior waypoints, making an 's' curve path
-            List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-            // End 3 meters straight ahead of where we started, facing forward
-            new Pose2d(3, 0, new Rotation2d(0)),
-            config);
-
-    MecanumControllerCommand mecanumControllerCommand =
-        new MecanumControllerCommand(
-            exampleTrajectory,
-            driveTrain::getPose,
-            AutoConstants.kFeedforward,
-            OdometryConstants.mecanumDriveKinematics,
-
-            // Position controllers
-            new PIDController(AutoConstants.kPXController, 0, 0),
-            new PIDController(AutoConstants.kPYController, 0, 0),
-            new ProfiledPIDController(
-                AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints),
-
-            // Needed for normalizing wheel speeds
-            AutoConstants.kMaxSpeedMetersPerSecond,
-
-            // Velocity PID's
-            new PIDController(AutoConstants.kPFrontLeftVel, 0, 0),
-            new PIDController(AutoConstants.kPRearLeftVel, 0, 0),
-            new PIDController(AutoConstants.kPFrontRightVel, 0, 0),
-            new PIDController(AutoConstants.kPRearRightVel, 0, 0),
-            driveTrain::getWheelSpeeds,
-            driveTrain::setDriveMotorControllersVolts, // Consumer for the output motor voltages
-            driveTrain);
-
+  public Command getAutonomousCommand() {  
     // Reset odometry to the initial pose of the trajectory, run path following
     // command, then stop at the end.
-    return Commands.sequence(
-        new InstantCommand(() -> driveTrain.resetOdometry(exampleTrajectory.getInitialPose())),
-        mecanumControllerCommand,
-        new InstantCommand(() -> driveTrain.drive(0, 0, 0)));
+    return Autos.exampleRun(driveTrain);
   }
 }
